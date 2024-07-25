bc7b9157c585409dc42dd8d3e50b319650307fe2
Beth Wickerson <beth.wickerson@signalthoery.com>
2024-07-25 10:42:11 -0500

    cursor scroll, nav active state

--- src/app/components/Cursor.js
+++ src/app/components/Cursor.js
@@ -3,30 +3,45 @@ import { useEffect } from "react";
 import styles from './Cursor.module.css'
 
 function Cursor() {
-    useEffect(() => {
-        const cursor = document.getElementById('custom-cursor');
-        const handleMouseMove = (event) => {
-          cursor.style.left = `${event.pageX}px`;
-          cursor.style.top = `${event.pageY}px`;
-        };
+  useEffect(() => {
+    const cursor = document.getElementById('custom-cursor');
+    document.body.style.cursor = 'none'; 
 
-        const handleMouseDown = () => {
-            cursor.classList.add(styles.active);
-        };
-    
-        const handleMouseUp = () => {
-            cursor.classList.remove(styles.active);
-        };
-        document.addEventListener('mousemove', handleMouseMove);
-        document.addEventListener('mousedown', handleMouseDown);
-        document.addEventListener('mouseup', handleMouseUp);
-    
-        return () => {
-          document.removeEventListener('mousemove', handleMouseMove);
-          document.removeEventListener('mousedown', handleMouseDown);
-          document.removeEventListener('mouseup', handleMouseUp);
-        };
-      }, []);
+    const handleMouseMove = (event) => {
+      cursor.style.left = `${event.pageX}px`;
+      cursor.style.top = `${event.pageY}px`;
+    };
+
+    const handleMouseDown = () => {
+        cursor.classList.add(styles.active);
+    };
+
+    const handleMouseUp = () => {
+        cursor.classList.remove(styles.active);
+    };
+
+    const handleScroll = () => {
+        cursor.style.transform = ' translate(-23px, -42px) scale(0)';
+        document.body.style.cursor = 'default';
+        clearTimeout(window.scrollTimeout);
+        window.scrollTimeout = setTimeout(() => {
+            cursor.style.transform = ' translate(-23px, -42px) scale(1)';
+            document.body.style.cursor = 'none';
+        }, 50); // Adjust the timeout as needed
+    };
+
+    document.addEventListener('mousemove', handleMouseMove);
+    document.addEventListener('mousedown', handleMouseDown);
+    document.addEventListener('mouseup', handleMouseUp);
+    document.addEventListener('scroll', handleScroll);
+
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+      document.removeEventListener('mousedown', handleMouseDown);
+      document.removeEventListener('mouseup', handleMouseUp);
+      document.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
     return (
         <div id="custom-cursor" className={styles.customCursor} />
     )
--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,4 +1,5 @@
 'use client';
+import { usePathname } from 'next/navigation'
 import { useEffect, useState, useContext } from 'react';
 import Link from 'next/link';
 import styles from "./Navbar.module.css";
@@ -6,7 +7,8 @@ import { AppContext } from '../context/AppContext';
 import { useMyState } from '../context/useMyState';
 
 function Navbar() {
-
+    const pathname = usePathname();
+    const isActive = (path) => pathname === path;
     // handle selectedState functions
     const { setMyState } = useContext(AppContext); 
     const { selectedMyState } = useMyState();
@@ -14,16 +16,6 @@ function Navbar() {
 
     const handleStateSelect = (state) => {
         setMyState(state);
-    };
-
-    const handleLinkClick = (e, state) => {
-        if (!selectedMyState) {
-            console.log('pick a state first')
-            e.preventDefault();
-            // setShowPopup(true);
-        } else {
-            // handleStateSelect(state);
-        }
     };
 
     // handle navbar background color on scroll
@@ -50,30 +42,30 @@ function Navbar() {
 
     return (
         <>
-            <nav className={`${styles.navbar} ${scrolled ? styles.scrolled : ''}`}>
+             <nav className={`${styles.navbar} ${scrolled ? styles.scrolled : ''}`}>
                 <ul className={styles.navbarWrapper}>
                     <li>
-                        <Link href="/" className={styles.logo}><span className='screen-reader-only'>HOME</span></Link>
+                        <Link href="/" className={`${styles.logo} ${isActive('/') ? styles.active : ''}`}><span className='screen-reader-only'>HOME</span></Link>
                     </li>
                     <li>
-                        <Link href="/voting-in-missouri" onClick={() => handleStateSelect('voting-in-missouri')}>VOTING IN MISSOURI</Link>
+                        <Link href="/voting-in-missouri" className={isActive('/voting-in-missouri') ? styles.active : ''} onClick={() => handleStateSelect('voting-in-missouri')}>VOTING IN MISSOURI</Link>
                     </li>
                     <li>
-                        <Link href="/voting-in-kansas" onClick={() => handleStateSelect('voting-in-kansas')}>VOTING IN KANSAS</Link>
+                        <Link href="/voting-in-kansas" className={isActive('/voting-in-kansas') ? styles.active : ''} onClick={() => handleStateSelect('voting-in-kansas')}>VOTING IN KANSAS</Link>
                     </li>
                     <li>
-                        <Link href="/voting-in-texas" onClick={() => handleStateSelect('voting-in-texas')}>VOTING IN TEXAS</Link>
+                        <Link href="/voting-in-texas" className={isActive('/voting-in-texas') ? styles.active : ''} onClick={() => handleStateSelect('voting-in-texas')}>VOTING IN TEXAS</Link>
                     </li>
                     {isMounted && selectedMyState && (
                         <>
                             <li>
-                                <Link href={`/${selectedMyState.toLowerCase()}#faqs`} onClick={(e) => handleLinkClick(e, selectedMyState)}>FAQs</Link>
+                                <Link href={`/${selectedMyState.toLowerCase()}#faqs`} className={isActive(`/${selectedMyState.toLowerCase()}#faqs`) ? styles.active : ''}>FAQs</Link>
                             </li>
                             <li>
-                                <Link href={`/${selectedMyState.toLowerCase()}#make-a-plan`} onClick={(e) => handleLinkClick(e, selectedMyState)}>MAKE A PLAN</Link>
+                                <Link href={`/${selectedMyState.toLowerCase()}#make-a-plan`} className={isActive(`/${selectedMyState.toLowerCase()}#make-a-plan`) ? styles.active : ''}>MAKE A PLAN</Link>
                             </li>
                             <li>
-                                <Link href={`/${selectedMyState.toLowerCase()}#outreach`} onClick={(e) => handleLinkClick(e, selectedMyState)}>BHB OUTREACH</Link>
+                                <Link href={`/${selectedMyState.toLowerCase()}#outreach`} className={isActive(`/${selectedMyState.toLowerCase()}#outreach`) ? styles.active : ''}>BHB OUTREACH</Link>
                             </li>
                         </>
                     )}
